13413d9aabece4cfc18e8436468eec348029bfd1
pythonstory <pincoin.info@gmail.com>
2016-06-08 01:56:20 +0900

    SQLAlchemy 연동 설정

---
+++ Flask-SQLAlchemy/01-config/hello.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# 초기화
+app = Flask(__name__)
+
+# 초기화 - SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' \
+    + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+# 뷰 함수와 라우팅
+@app.route('/')
+def index():    
+    return '<h1>Hello World!</h1>'
+
+# 서버 실행
+if __name__ == '__main__':
+    app.host = '127.0.0.1'
+    app.port = 5000
+    app.debug = True
+    
+    app.run()
--- Flask-SQLAlchemy/README.md
+++ Flask-SQLAlchemy/README.md
@@ -1,9 +1,21 @@
+# Flask-SQLAlchemy
+
 [Flask-SQLAlchemy 공식문서](http://flask-sqlalchemy.pocoo.org/2.1/)
 
-# 연동 설정
+## 연동 설정
 
-# 모델 정의
+### 연동 설정
 
-# 관계 정의
+* SQLALCHEMY_DATABASE_URI
+    * sqlite:////absolute/path/to/foo.db
+    * mysql://scott:tiger@localhost/mydatabase
+    * oracle://scott:tiger@127.0.0.1:1521/sidname
+    * postgresql://scott:tiger@localhost/mydatabase
+* SQLALCHEMY_TRACK_MODIFICATIONS
+    * 2.1 버전부터는 명시적으로 False로 지정할 것
+    
+## 모델 정의
 
-# CRUD
+## 관계 정의
+
+## CRUD
