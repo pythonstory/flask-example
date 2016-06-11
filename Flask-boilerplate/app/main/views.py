125e36535cfa7e2d9a7b26301df62fc97a104bee
pythonstory <pincoin.info@gmail.com>
2016-06-12 03:18:50 +0900

    Flask-Babel 번역 설정

--- Flask-boilerplate/app/main/views.py
+++ Flask-boilerplate/app/main/views.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
-from flask_babel import format_datetime
-from datetime import datetime
+from flask_babel import gettext
 
 from . import main
 
 
 @main.route('/')
 def index():
-
-    print(format_datetime(datetime(1987, 3, 5, 17, 12)))
-
+    print(gettext('Home'))
     return render_template('default/main/index.html')
